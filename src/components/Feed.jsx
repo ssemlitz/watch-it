c8c76eca0ba5ef697dba9ec3ce0ee99bee1791f7
ssemlitz <sdsemlitz@yahoo.com>
2022-10-21 12:41:08 -0400

    stub up video and channel cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,9 +8,11 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 const Feed = () => {
   
   const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
-    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
   }, [selectedCategory])  
   
   return (
@@ -28,7 +30,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
           {selectedCategory} <span style={{ color: '#FC1503'}}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
